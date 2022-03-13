5cd699be4b48182a0006d5fc4e160a574a79a119
hstla <ree7243@gmail.com>
2022-03-13 20:51:41 +0900

    hello World 메세지 삭

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,6 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello World");
-		System.out.println("hello World");
 		int a = 1;
 		System.out.println(a);
 	}
