d6fb53fc9a82a40903bb8407965a866e196dcabb
hstla <ree7243@gmail.com>
2022-03-13 20:33:13 +0900

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hello World");
+		System.out.println("hello World");
+		int a = 1;
+		System.out.println(a);
+	}
+}
