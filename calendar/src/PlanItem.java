11312302c3ffd7ebbdbe95d07ba030fd2cc07c8f
hstla <ree7243@gmail.com>
2022-04-02 22:44:58 +0900

    Add_datebase

--- calendar/src/PlanItem.java
+++ calendar/src/PlanItem.java
@@ -27,5 +27,11 @@ public class PlanItem {
     public void addPeople(String name) {
         peoples += name + ",";
     }
+
+    public String saveString() {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        String sdate = formatter.format(planDate);
+        return sdate+","+"\""+detail+"\""+"\n";
+    }
 }
 
